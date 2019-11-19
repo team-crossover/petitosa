b7b00c067cad2bb4d05fea4273c2673367e03977
NataliaLopesSilva <natalialopescig@gmail.com>
2019-11-19 00:10:02 -0300

    Conclusão das telas de ver perfil de contratante e prestador de serviço.

--- src/app/perfil-contratante/perfil-contratante.component.ts
+++ src/app/perfil-contratante/perfil-contratante.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Contratante } from '../_models';
+import { ContratanteService } from '../_services';
 
 @Component({
   selector: 'app-perfil-contratante',
@@ -8,10 +10,16 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class PerfilContratanteComponent implements OnInit {
 
+  contratante: Contratante;
+  error: any;
+  servicosPrestados: boolean[] = [];
+  precos: number[] = [];
+
   public idContratante: number;
 
   constructor(
     private route: ActivatedRoute,
+    private contratanteService: ContratanteService
   ) {
     this.route.params.subscribe(params => {
       if (params['id']) {
@@ -21,6 +29,19 @@ export class PerfilContratanteComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.contratanteService.getContratante(this.idContratante).subscribe(data => {
+      this.contratante = data;
+
+      if (this.contratante.genero === "M") {
+        this.contratante.genero = "Masculino";
+      }
+      else if (this.contratante.genero === "F") {
+        this.contratante.genero = "Feminino";
+      }
+      else {
+        this.contratante.genero = "Outros";
+      }
+    });
   }
 
 }
--- src/app/perfil-prestador/perfil-prestador.component.ts
+++ src/app/perfil-prestador/perfil-prestador.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Prestador } from '../_models';
+import { PrestadorService } from '../_services';
 
 @Component({
   selector: 'app-perfil-prestador',
@@ -9,9 +11,14 @@ import { ActivatedRoute } from '@angular/router';
 export class PerfilPrestadorComponent implements OnInit {
 
   public idPrestador: number;
+  prestador: Prestador;
+  error: any;
+  servicosPrestados: boolean[] = [];
+  precos: number[] = [];
 
   constructor(
     private route: ActivatedRoute,
+    private prestadorService: PrestadorService
   ) {
     this.route.params.subscribe(params => {
       if (params['id']) {
@@ -21,6 +28,20 @@ export class PerfilPrestadorComponent implements OnInit {
   }
 
   ngOnInit() {
-  }
+    this.prestadorService.getPrestador(this.idPrestador).subscribe(data => {
+      this.prestador = data;
+      this.precos = this.prestador.precos;
+      this.servicosPrestados = this.prestador.servicosPrestados;
 
+      if (this.prestador.genero === "M") {
+        this.prestador.genero = "Masculino";
+      }
+      else if (this.prestador.genero === "F") {
+        this.prestador.genero = "Feminino";
+      }
+      else {
+        this.prestador.genero = "Outros";
+      }
+    });
+  }
 }
